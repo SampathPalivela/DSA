c8590c1c239b8241112ade69d2262e4be7626a57
SampathPalivela <sampathpalivela@gmail.com>
2023-12-04 23:46:08 +0530

    find path in bst  from node to root

---
+++ BST/6_Find_path_in_BST.cpp
@@ -0,0 +1,119 @@
+
+#include <iostream>
+#include <queue>
+#include <vector>
+using namespace std;
+
+
+template <typename T>
+class BinaryTreeNode {
+   public:
+    T data;
+    BinaryTreeNode<T> *left;
+    BinaryTreeNode<T> *right;
+
+    BinaryTreeNode(T data) {
+        this->data = data;
+        left = NULL;
+        right = NULL;
+    }
+    ~BinaryTreeNode() {
+        if (left) delete left;
+        if (right) delete right;
+    }
+};
+
+
+
+BinaryTreeNode<int> *takeInput() {
+    int rootData;
+    cin >> rootData;
+    if (rootData == -1) {
+        return NULL;
+    }
+
+    BinaryTreeNode<int> *root = new BinaryTreeNode<int>(rootData);
+    queue<BinaryTreeNode<int> *> q;
+    q.push(root);
+    while (!q.empty()) {
+        BinaryTreeNode<int> *currentNode = q.front();
+        q.pop();
+        int leftChild, rightChild;
+        cin >> leftChild;
+        if (leftChild != -1) {
+            BinaryTreeNode<int> *leftNode = new BinaryTreeNode<int>(leftChild);
+            currentNode->left = leftNode;
+            q.push(leftNode);
+        }
+        cin >> rightChild;
+        if (rightChild != -1) {
+            BinaryTreeNode<int> *rightNode =
+                new BinaryTreeNode<int>(rightChild);
+            currentNode->right = rightNode;
+            q.push(rightNode);
+        }
+    }
+
+    return root;
+}
+
+
+//CODE
+vector<int>* getPath(BinaryTreeNode<int>* root, int data) {
+    if (root == NULL) {
+        vector<int>* x = new vector<int>();
+        return x;
+    }
+
+    if (root->data == data) {
+        vector<int>* x = new vector<int>{root->data};
+        return x;
+    }
+
+    if (root->left == NULL && root->right == NULL) {
+        vector<int>* x = new vector<int>();
+        return x;
+    }
+
+    if (root->data > data) {
+        if (!root->left) {
+            vector<int>* x = new vector<int>();
+            return x;
+        } else {
+            vector<int>* x = getPath(root->left, data);
+			if(x->empty()){
+				return x;
+			}
+            x->push_back(root->data);
+            return x;
+        }
+    }
+
+    if (root->data < data) {
+        if (!root->right) {
+            vector<int>* x = new vector<int>();
+            return x;
+        } else {
+            vector<int>* x = getPath(root->right, data);
+			if(x->empty()){
+				return x;
+			}
+            x->push_back(root->data);
+            return x;
+        }
+    }
+}
+int main() {
+    BinaryTreeNode<int> *root = takeInput();
+    int k;
+    cin >> k;
+    vector<int> *output = getPath(root, k);
+
+    if (output != NULL) {
+        for (int i = 0; i < output->size(); i++) {
+            cout << output->at(i) << " ";
+        }
+    }
+
+    delete root;
+}
